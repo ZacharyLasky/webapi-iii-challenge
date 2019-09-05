569a17c297dbc63d6dc4615fe69f4a47233fd533
Zach Lasky <zacharylasky123@gmail.com>
2019-09-05 15:49:52 -0400

    port changes

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // code away!
+// require('dotenv').config()
+
 const server = require("./server");
-server.listen(5000, () => {
-  console.log("API on port 5000");
+
+const port = process.env.PORT || 5000
+
+server.listen(port, () => {
+  console.log(`API on port ${port}`);
 });
